bd0ce7200bdbdfa045f69f6765312dff92c41154
pavel.brylev <pavel.brylev@supremedesign.ru>
2016-02-01 14:44:12 +0300

    доведение до рабочего состояния, package.json update

--- index.js
+++ index.js
@@ -3,22 +3,36 @@ var   router = require( "koa-router" )(),
       SocketStream = require( "./lib/socket-stream" ),
       BrowserChannelServer = require("browserchannel").server,
       connect = require("browserchannel/node_modules/connect");
+      WebSocket = require('koa-websocket/node_modules/ws');
 
 function Transport( racerStore, options ){
    this.racerStore = racerStore;
    this.options = options;
 }
 Transport.prototype.connect = function( app ){
+   var self = this;
    var sessionEnable = !!this.options.session;
    var racerStore = this.racerStore;
 
    // web socket
    websockify( app );
 
-   router.get( this.options.base, function *( next ){
-      var stream = new SocketStream( this.websocket );
+   router.get( this.options.channelName, function *( next ){
+      var client = this.websocket;
+      var stream = new SocketStream( client );
       if(this.session) stream.session = this.session;
-      racerStore.listen( stream, this.websocket.upgradeReq );
+      racerStore.listen( stream, client.upgradeReq );
+
+      if (!self.options.noPing){
+         client.timer = setInterval(function(){
+            if (client.readyState === WebSocket.OPEN) {
+               client.ping();
+            } else {
+               clearInterval(client.timer);
+            }
+         }, self.options.pingInterval);
+      }
+
       yield next;
    });
 
@@ -27,6 +41,7 @@ Transport.prototype.connect = function( app ){
    app.ws.use( router.routes() );
 
    // browserchannel
+   this.options.base = this.options.channelName;
    var BCmiddleware = BrowserChannelServer(this.options, function (client, connectRequest) {
       if(connectRequest.session) client.session = connectRequest.session;
       var stream = new SocketStream( client );
